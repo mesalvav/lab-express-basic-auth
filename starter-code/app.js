5cf9ae9921cafba3ac23a3fa6c3da71393a89388
mario salvatierra <msalv006@fiu.edu>
2019-07-07 21:21:45 -0400

    added signup

--- starter-code/app.js
+++ starter-code/app.js
@@ -11,7 +11,7 @@ const path         = require('path');
 
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/expressBasicAuthDB', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -47,12 +47,13 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = 'My Basic Auth';
 
 
 
-const index = require('./routes/index');
-app.use('/', index);
-
+// const index = require('./routes/index');
+// app.use('/', index);
+const router = require('./routes/auth');
+app.use('/', router);
 
 module.exports = app;
